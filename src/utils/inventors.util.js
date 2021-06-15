92a67d4358e030fb7cf64cc8cf8f34aaeb9f2ecc
pedroe.ramos <pedroe.ramos@softtek.com>
2021-06-15 13:08:04 -0500

    functions examples

--- src/utils/inventors.util.js
+++ src/utils/inventors.util.js
@@ -1,3 +1,7 @@
+// Array.prototype.map()
+// 2. Give us an array of the inventor first and last names
+export const fullNames = inventor => inventor.map(`${inventor.first} ${inventor.last}`);
+
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
 export const totalYears = inventor =>
