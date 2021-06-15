03f3bdd2ea7c63f0c6c579a5de2638faafce97a3
pedroe.ramos <pedroe.ramos@softtek.com>
2021-06-15 13:50:00 -0500

    function correct ejecute

--- src/utils/inventors.util.js
+++ src/utils/inventors.util.js
@@ -1,10 +1,10 @@
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-export const fifteen = inventor => inventor.filter(inventor.year >= 1500 && inventor.year < 1600);
+export const fifteen = inventor => inventor.filter((index) => index.year >= 1500 && index.year < 1600);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventor first and last names
-export const fullNames = inventor => inventor.map(`${inventor.first} ${inventor.last}`);
+export const fullNames = inventor => inventor.map(i => `${i.first} ${i.last}`);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
